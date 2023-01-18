94cef8250d036f35ce4a764d1d1bf848d64999a2
1Dohyeon <dh1072005@gmail.com>
2023-01-18 18:08:54 +0900

    commit

--- Algorithms(python)/02_baekjoon/14_brute_force2/159_N과M_2_15650.py
+++ Algorithms(python)/02_baekjoon/14_brute_force2/159_N과M_2_15650.py
@@ -9,11 +9,8 @@ def dfs(s):
     for i in range(s,n+1):  
         if i not in num:   
             num.append(i)
-            print(num)
-            dfs(i+1)    # i를 이용하여 자신의 다음 숫자를 불러 중복을 없앤다. 
-            print(num)         
+            dfs(i+1)    # i를 이용하여 자신의 다음 숫자를 불러 중복을 없앤다.          
             num.pop()    
-            print(num)  
 
 dfs(1)
 # txt파일에 임의의 n,m을 주어 함수 진행을 살펴봄
