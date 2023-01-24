2626cb2405baf10c443276de9d583020b914e749
1Dohyeon <dh1072005@gmail.com>
2023-01-24 12:58:06 +0900

    commit

---
+++ Algorithms(python)/00_review/01_solve/01_11047.py
@@ -0,0 +1,14 @@
+n,k=map(int,input().split())
+coins=[]
+
+for _ in range(n):
+    coins.append(int(input()))
+
+cnt=0
+for i in range(n-1,-1,-1):
+    cnt+=k//coins[i]
+    k%=coins[i]
+    if k==0:
+        print(cnt)
+        break
+
