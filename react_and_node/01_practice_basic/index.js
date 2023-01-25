057c7b520f8e1e0ed8f352ba644f1f73bea7abd2
1Dohyeon <dh1072005@gmail.com>
2023-01-26 01:00:18 +0900

    commit

--- react_and_node/01_practice_basic/index.js
+++ react_and_node/01_practice_basic/index.js
@@ -1,6 +1,12 @@
 const express = require('express')  // express 모듈 가져옴
 const app = express()  
-const port = 3000
+const port = 5000
+
+const mongoose=require('mongoose')
+mongoose.connect('mongodb+srv://dohyeon:dh2417@01basic.gh3kwmo.mongodb.net/?retryWrites=true&w=majority',{
+   
+}).then(()=>console.log('MongoDB Connected...'))
+  .catch(err=>console.log(err))
 
 app.get('/', (req, res) => {
   res.send('Hello World! ~ 안녕하세요') // npm run start
