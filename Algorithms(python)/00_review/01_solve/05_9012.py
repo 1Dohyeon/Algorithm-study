819dcb2fe17531491de09f8151d1e4c78ba65a99
1Dohyeon <dh1072005@gmail.com>
2023-01-26 12:58:59 +0900

    commit

--- Algorithms(python)/00_review/01_solve/05_9012.py
+++ Algorithms(python)/00_review/01_solve/05_9012.py
@@ -31,7 +31,8 @@ for _ in range(t):
             stack.append(i)
         elif i==')' and stack:
             stack.pop()
-        elif i==')' and len(stack)==0:
+        elif i==')' and len(stack)==0:  # stack[-1]=='('으로 조건을 뒀을 경우 에러가 뜸. stack에 아무것도 없을 경우
+                                        # stack[-1]이 대입될 수 없으므로 에러가 뜨는 거 같음
             print('NO')
             break
     else:
