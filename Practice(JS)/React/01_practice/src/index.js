7d1f4e0889421d3acf568c7c50d7d74917568e47
1Dohyeon <dh1072005@gmail.com>
2023-01-30 22:37:20 +0900

    commit

--- Practice(JS)/React/01_practice/src/index.js
+++ Practice(JS)/React/01_practice/src/index.js
@@ -5,17 +5,17 @@ import reportWebVitals from './reportWebVitals';
 // import App from './App';
 // import Library from './chapter_03/Library';
 // import Clock from './chapter_04/Clock';
-import CommentList from './chapter_05/CommentList';
-
+// import CommentList from './chapter_05/CommentList';
+import NotificationList from './chapter_06/NotificationList';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CommentList/>
+    <NotificationList/>
   </React.StrictMode>,
   document.getElementById('root')
 )
 /* 
-chapter_03  Clock.jsx에서 변화되는 부분을 setInterval 함수를 이용하여 1000ms마다 render
+chapter_04  Clock.jsx에서 변화되는 부분을 setInterval 함수를 이용하여 1000ms마다 render
 const root = ReactDOM.createRoot(document.getElementById('root'));
 setInterval(()=>{ 
   root.render(
