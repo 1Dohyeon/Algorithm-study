e0a683e75e9d8b47b22bc218299efd990c32ef02
1Dohyeon <dh1072005@gmail.com>
2023-01-31 15:37:44 +0900

    commit

--- Practice(JS)/React/02_practice/src/index.js
+++ Practice(JS)/React/02_practice/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import CityList from './chapter_01/CityList';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <CityList />
   </React.StrictMode>
 );
 
